06b77fd0dccff072251cf1f5771f5ffa22c2de5e
Hoshibi <tanyshiotani@hotmail.com>
2022-11-23 20:39:16 -0300

    feat: criação do projeto inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="HomeContainer">
+      <h3>Projeto de Arquitetura</h3>
+      <p>Configuração do corpo inicial do projeto</p>
+    </div>
+  );
+}
+
+export default App;
