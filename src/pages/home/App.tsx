ad2780a3c80631e9808e55be0eab925879df965d
iasyl <iasyl@outlook.com>
2022-12-02 20:15:43 -0300

    Esqueleto base
    Apenas fiz uma base para ir ajudando a visualizar.

--- src/pages/home/App.tsx
+++ src/pages/home/App.tsx
@@ -4,6 +4,29 @@ import "./App.css";
 function App() {
   return (
     <div className="HomeContainer">
+      <div id="MainScreen">
+        <button>
+          + Adicionar Programa
+        </button>
+
+        <div className="List">
+          Programas no Sistema
+          <div>
+
+          </div>
+          <ul>
+            <li>ID</li>
+            <li>Nome Programa</li>
+            <li>Data</li>
+            <li>Gestor</li>
+            <button>Gerenciar Programa</button>
+            <button>Excluir Programa</button>
+          </ul> 
+          <div className= "Tabela">
+            
+          </div>
+        </div>
+      </div>
       <h3>Projeto de Arquitetura</h3>
       <p>Configuração do corpo inicial do projeto</p>
       <button onClick={() => toast.success("mensagem")}>Toast</button>
