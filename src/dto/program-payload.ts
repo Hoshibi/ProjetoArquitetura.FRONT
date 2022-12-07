24d18154475b552ac558dda0fac848eddb76bbe9
Hoshibi <tanyshiotani@hotmail.com>
2022-12-07 02:31:48 -0300

    feat: criação do dto e interface da entidade Program

---
+++ src/dto/program-payload.ts
@@ -0,0 +1,8 @@
+export interface ProgramPayload {
+    NomePrograma: string,
+    Gestor: string,
+    Objetivo: string,
+    Justificativa: string,
+    Envolvidos: string,
+    Procedimento: string
+}
---
+++ src/interface/program.interface.ts
@@ -0,0 +1,10 @@
+export interface ProgramType {
+    Id: number
+    NomePrograma: string,
+    Gestor: string,
+    Data: string
+    Objetivo: string,
+    Justificativa: string,
+    Envolvidos: string,
+    Procedimento: string
+}
